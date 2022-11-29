a55490b9fb4d264726ac77a292d1f5f1b26009c2
rae49yuki <hp_cx@hotmail.com>
2022-11-29 14:13:57 +0800

    set up flask app for FE/BE integration. updated readme

--- Flask App/main.py
+++ Flask App/main.py
@@ -1,21 +1,69 @@
 from flask import Flask, render_template, request, url_for, redirect
+from CF import similar_5movies
 
 app = Flask(__name__)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        name = request.form["name"]
-        return redirect(url_for('results', name=name))
+        search = request.form["search"]
+        return redirect(url_for('results', search=search))
     return render_template('index.html')
-
 
 @app.route("/results", methods=['GET', 'POST'])
 def results():
-    name = request.form.get('name')
-    print(name)
-    return render_template("results.html", name=name)
+    search = request.form.get('search') # this is the search term
+
+    #################################################
+    # add back-end processing codes for search here #
+    # input: search (string)
+    # output: list of dictionary items, variables = title, year, director, cast, description 
+    #################################################
+
+    results = [
+        {
+            'title': 'Movie Title 1',
+            'year': '2022',
+            'director': 'Director 1',
+            'cast': 'Cast1, Cast2',
+            'description': 'Description 1'
+        },
+
+        {
+            'title': 'Movie Title 2',
+            'year': '2021',
+            'director': 'Director 2',
+            'cast': 'Cast1, Cast2',
+            'description': 'Description 2'
+        }
+    ]
+    length=len(results)
+
+    ##########################################################
+    # add back-end processing codes for recommendations here #
+    # input: search (string)
+    # output: list of dictionary items, variables = title, year, director, cast, description 
+    ##########################################################
+    #results = similar_5movies('Dirty Dancing (1987)')
+
+    recommendations = [
+        {
+            'title': 'Movie Title 3',
+            'year': '2020',
+            'director': 'Director 3',
+            'cast': 'Cast1, Cast2',
+            'description': 'Description 3'
+        },
+
+        {
+            'title': 'Movie Title 4',
+            'year': '2019',
+            'director': 'Director 4',
+            'cast': 'Cast1, Cast2',
+            'description': 'Description 4'
+        }
+    ]
+    return render_template("results.html", search=search, length=length, results=results, recommendations=recommendations)
 
 if __name__ == "__main__":
     app.run()
