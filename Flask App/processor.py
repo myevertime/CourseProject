5c9804056b4ea8cbe447075ad3e5356be5256ca7
Julie <39979050+myevertime@users.noreply.github.com>
2022-12-05 23:36:39 +0900

    Create processor.py

---
+++ Flask App/processor.py
@@ -0,0 +1,56 @@
+from CF import similar_5movies
+from knowledge_graph import knowledge_graph
+from searcher import search
+
+def processor(keyword=''):
+
+    import pandas as pd
+
+    search_result = search(keyword)
+
+    metadata = pd.read_csv("output_rendered.csv")
+
+    ##### Search Results
+    results = []
+    for i in range(len(search_result)):
+        result = {}
+        result['title'] = metadata.loc[metadata['movieId'] == search_result[i], 'title'].item()
+        result['genres'] = metadata.loc[metadata['movieId'] == search_result[i], 'genres'].item()
+        result['director'] = metadata.loc[metadata['movieId'] == search_result[i], 'director'].item()
+        result['actors'] = metadata.loc[metadata['movieId'] == search_result[i], 'actors'].item()
+        results.append(result)
+
+    ##### Recommendation Results
+    ## Get MovieIDs for KG
+    tmdb_id = str(metadata.loc[metadata['movieId'] == search_result[0], 'tmdbId'].item())
+    imdb_id = str(metadata.loc[metadata['movieId'] == search_result[0], 'imdbId'].item())
+
+    ## Get Top5 movie IDs from each model
+    CF_reco = similar_5movies(search_result[0])
+    kg_reco = knowledge_graph(tmdb_id, imdb_id)
+    
+    ## Map KG recommendation results to MovieID
+    kg_reco_pro = []
+    for i in kg_reco:
+        if "tt" not in i:
+            check = metadata.loc[metadata['tmdbId'] == int(i.replace("tt","")), 'movieId']
+            if len(check) > 0: # If ID exists in output.csv
+                kg_reco_pro.append(metadata.loc[metadata['tmdbId'] == int(i), 'movieId'].item())
+        else:
+            check = metadata.loc[metadata['imdbId'] == int(i.replace("tt","")), 'movieId']
+            if len(check) > 0: # If ID exists in output.csv
+                kg_reco_pro.append(metadata.loc[metadata['imdbId'] == int(i.replace("tt","")), 'movieId'].item())
+
+    reco = list(CF_reco)
+    reco.extend(x for x in kg_reco_pro if x not in reco) # extend without duplicates
+
+    recommendations = []
+    for i in range(len(reco)):
+        result = {}
+        result['title'] = metadata.loc[metadata['movieId'] == reco[i], 'title'].item()
+        result['genres'] = metadata.loc[metadata['movieId'] == reco[i], 'genres'].item()
+        result['director'] = metadata.loc[metadata['movieId'] == reco[i], 'director'].item()
+        result['actors'] = metadata.loc[metadata['movieId'] == reco[i], 'actors'].item()
+        recommendations.append(result)
+
+    return results , recommendations
